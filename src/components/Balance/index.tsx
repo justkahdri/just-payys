98a9efc03ff73c5e853ce154c669aa51a1f9c626
JustKahdri <joaco.rm512@gmail.com>
2021-10-27 00:12:07 -0300

    created Balance Page

---
+++ src/components/Balance/index.tsx
@@ -0,0 +1,36 @@
+import React, {useContext} from "react";
+import {RouteComponentProps, Redirect} from "@reach/router";
+import {Heading, Stack, Text, Flex} from "@chakra-ui/layout";
+
+import {PeopleContext} from "../../contexts/PeopleProvider";
+
+const BalancePage = (_: RouteComponentProps) => {
+  const {people} = useContext(PeopleContext);
+
+  if (people.length < 2) return <Redirect noThrow to="/people" />;
+
+  return (
+    <Stack>
+      {people
+        .filter((p) => p.personal_balance !== 0)
+        .map((person) => (
+          <Flex
+            key={person.id}
+            align="center"
+            justify="space-between"
+            p={2}
+            rounded="sm"
+            shadow="md"
+          >
+            <Text as="h4">{person.name}</Text>
+            <Text color={person.personal_balance > 0 ? "secondary.300" : "red.300"} fontSize="sm">
+              {person.personal_balance > 0 ? "Recibe " : "Debe "}$
+              {Math.abs(person.personal_balance)}
+            </Text>
+          </Flex>
+        ))}
+    </Stack>
+  );
+};
+
+export default BalancePage;
